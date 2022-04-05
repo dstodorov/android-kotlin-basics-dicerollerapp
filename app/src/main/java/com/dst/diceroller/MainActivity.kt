1072cba3498deb44282b4a45b8d22c5ac9e88caf
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-04-05 15:54:08 +0300

    Add roll dice functionality

--- app/src/main/java/com/dst/diceroller/MainActivity.kt
+++ app/src/main/java/com/dst/diceroller/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.dst.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = Dice(6).roll().toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
